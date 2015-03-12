0bb4f3d602d8565bbf500a880f563a2b1fa47d1d
mar-v-in <github@rvin.mooo.com>
2015-03-13 00:46:00 +0100

    Add support for gradle structure to Android.mk

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := SafeParcel
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
