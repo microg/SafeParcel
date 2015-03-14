454265fd3ee465cc0741a57f3a74a6e747c2fcd5
mar-v-in <github@rvin.mooo.com>
2015-03-14 21:58:51 +0100

    Put source into submodule

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := SafeParcel
-LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java)
+LOCAL_SRC_FILES := $(call all-java-files-under, safe-parcel/src/main/java)
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
