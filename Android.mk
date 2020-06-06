efb2c7f09f8f6d8cd2af295545314c1596df77a3
Marvin W <git@larma.de>
2020-06-06 12:43:19 +0200

    Update build and copyright hints

--- Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := SafeParcel
-LOCAL_SRC_FILES := $(call all-java-files-under, safe-parcel/src/main/java)
-
-include $(BUILD_STATIC_JAVA_LIBRARY)
--- safe-parcel/src/test/java/org/microg/safeparcel/test/mock/MockParcel.java
+++ safe-parcel/src/test/java/org/microg/safeparcel/test/mock/MockParcel.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2019, microG Project Team
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.microg.safeparcel.test.mock;
 
 import android.os.Parcel;
